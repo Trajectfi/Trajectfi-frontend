642e5dcbda495023ddfbac686401e7cd7d620ae3
soomtochukwu <onwuajuesesomtochukwu@gmail.com>
2025-05-01 19:23:11 +0100

    ++

--- dapp/components/_components/SideBar.tsx
+++ dapp/components/_components/SideBar.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import React from "react";
 import Logo from "./Logo";
 import Link from "next/link";
@@ -9,45 +9,52 @@ import Icon from "./Icon";
 import { usePathname } from "next/navigation";
 
 const navs = [
-    {
-        url: "/borrow/assets",
-        name: "My assets",
-    },
-    {
-        url: "/borrow/get_loan",
-        name: "Get a Loan",
-    },
-    {
-        url: "/borrow/give_loan",
-        name: "Give a Loan",
-    },
+  {
+    url: "/borrow/assets",
+    name: "My assets",
+  },
+  {
+    url: "/borrow/get_loan",
+    name: "Get a Loan",
+  },
+  {
+    url: "/borrow/give_loan",
+    name: "Give a Loan",
+  },
 ];
 
 const SideBar = () => {
-    const path = usePathname();
-    return (
-        <div className="border-r-2 h-screen border-[#131429]  w-80 p-3 space-y-12">
-            <div className="mt-5">
-                <Logo />
-            </div>
-            <Search />
-            <div className="flex flex-col space-y-2">
-                {navs.map((nav, index) => {
-                    return (
-                        <Link className={"flex rounded-xl p-3 items-center space-x-4" + " " + (path.includes(nav.url) ? "bg-[#835aff]" : " ")} href={nav.url} key={index}>
-                            <div>
-                                <Icon src={"/images/icons/dashboard-square.svg"} />
-                            </div>
-                            <div>
-
-                                {nav.name}
-                            </div>
-                        </Link>
-                    );
-                })}
-            </div>
-        </div>
-    );
+  const path = usePathname();
+  return (
+    <div className="border-r-2 h-screen border-[#131429]  w-80 p-3 space-y-12">
+      <div className="mt-5">
+        <Link href={"/"}>
+          <Logo />
+        </Link>
+      </div>
+      <Search />
+      <div className="flex flex-col space-y-2">
+        {navs.map((nav, index) => {
+          return (
+            <Link
+              className={
+                "flex rounded-xl p-3 items-center space-x-4" +
+                " " +
+                (path.includes(nav.url) ? "bg-[#835aff]" : " ")
+              }
+              href={nav.url}
+              key={index}
+            >
+              <div>
+                <Icon src={"/images/icons/dashboard-square.svg"} />
+              </div>
+              <div>{nav.name}</div>
+            </Link>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default SideBar;
